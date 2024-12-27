0a7754dd16d2300bf7153e51b5b7b16263455bda
wangzhizhou <wangzhizhou@bytedance.com>
2024-12-28 03:11:57 +0800

    openapi doc draft and pass one unit test

--- Sources/MojangAPI/Mojang.swift
+++ Sources/MojangAPI/Mojang.swift
@@ -2,12 +2,26 @@ import OpenAPIRuntime
 import OpenAPIURLSession
 
 public struct Mojang {
-    public static let manifestClient = Client(
+    
+    public static func fetchManifest() async throws -> Components.Schemas.Manifest {
+        let response = try await manifestClient.getMinecraftGameVersionManifest()
+        return try response.ok.body.json
+    }
+    
+    public static func fetchPackage(id: String, sha1: String) async throws -> Components.Schemas.Package {
+        let response = try await packageClient.getPackage(path: .init(id: id, sha1: sha1))
+        return try response.ok.body.json
+    }
+}
+
+extension Mojang {
+    
+    private static let manifestClient = Client(
         serverURL: try! Servers.Server1.url(),
         transport: URLSessionTransport()
     )
     
-    public static let gameInfoClient = Client(
+    private static let packageClient = Client(
         serverURL: try! Servers.Server2.url(),
         transport: URLSessionTransport()
     )
--- Tests/MojangAPITests/MojangAPITests.swift
+++ Tests/MojangAPITests/MojangAPITests.swift
@@ -1,7 +1,32 @@
 import Testing
-@testable import MojangAPI
+import MojangAPI
 
-@Test func FetchManifest() async throws {
-    let response = try await Mojang.manifestClient.getManifest(.init())
-    print(response)
+@Test
+func fetchManifest() async throws {
+    let json = try await Mojang.fetchManifest()
+    try #require(json.latest?.release?.isEmpty == false)
+    try #require(json.versions?.isEmpty == false)
+    try #require(json.versions?.first?.url?.isEmpty == false)
+}
+
+
+
+@Test(arguments: [
+    ("1.21.4", "a3bcba436caa849622fd7e1e5b89489ed6c9ac63"),
+    ("19", "87954e1d978096bd2cb1cd5a507a55fe25690a51"),
+])
+func fetchPackageInfo(id: String, sha1: String) async throws {
+    let response = try await Mojang.fetchPackage(id: id, sha1: sha1)
+    switch response {
+    case .Minecraft(let minecraft):
+        try #require(minecraft.arguments?.game?.isEmpty == false)
+        try #require(minecraft.arguments?.jvm?.isEmpty == false)
+        try #require(minecraft.assetIndex?.url?.isEmpty == false)
+        try #require(minecraft.downloads?.client?.url.isEmpty == false)
+        try #require(minecraft.downloads?.server?.url.isEmpty == false)
+        try #require(minecraft.libraries?.isEmpty == false)
+        try #require(minecraft.mainClass?.isEmpty == false)
+    case .Resource(let resources):
+        try #require(resources.objects?.value.isEmpty == false)
+    }
 }
