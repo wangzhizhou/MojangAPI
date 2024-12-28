b974694cf46dcd9bbb2f3714e9e89a29cbda4577
wangzhizhou <wangzhizhou@bytedance.com>
2024-12-28 14:03:04 +0800

    refine the api

--- Sources/MojangAPI/Mojang.swift
+++ Sources/MojangAPI/Mojang.swift
@@ -3,26 +3,54 @@ import OpenAPIURLSession
 
 public struct Mojang {
     
-    public static func fetchManifest() async throws -> Components.Schemas.Manifest {
-        let response = try await manifestClient.getMinecraftGameVersionManifest()
-        return try response.ok.body.json
-    }
-    
-    public static func fetchPackage(id: String, sha1: String) async throws -> Components.Schemas.Package {
-        let response = try await packageClient.getPackage(path: .init(sha1: sha1, id: id))
-        return try response.ok.body.json
-    }
-}
-
-extension Mojang {
-    
     private static let manifestClient = Client(
         serverURL: try! Servers.Server1.url(),
         transport: URLSessionTransport()
     )
     
+    public static func fetchManifest() async throws -> Components.Schemas.Manifest {
+        let response = try await manifestClient.getMinecraftGameVersionManifest()
+        return try response.ok.body.json
+    }
+    
+    public static func gameVersion(of version: Components.Schemas.Version) async throws -> Components.Schemas.GameVersion? {
+        guard case let .GameVersion(gameVersion) = try await fetchPackage(id: version.id, sha1: version.sha1)
+        else {
+            return nil
+        }
+        return gameVersion
+    }
+}
+
+extension Mojang {
+    
     private static let packageClient = Client(
         serverURL: try! Servers.Server2.url(),
         transport: URLSessionTransport()
     )
+    
+    static func fetchPackage(id: String, sha1: String) async throws -> Components.Schemas.Package {
+        let response = try await packageClient.getPackage(path: .init(sha1: sha1, id: id))
+        return try response.ok.body.json
+    }
+}
+
+extension Components.Schemas.Version {
+    public var sha1: String {
+        String(url.split(separator: "/").dropLast().last!)
+    }
+}
+
+extension Components.Schemas.GameVersion {
+    
+    public var assetIndexObject: Components.Schemas.GameVersionAsset? {
+        get async throws {
+            guard let assetId = assetIndex.id,
+                  case let .GameVersionAsset(gameAsset) = try await Mojang.fetchPackage(id: assetId, sha1: assetIndex.sha1)
+            else {
+                return nil
+            }
+            return gameAsset
+        }
+    }
 }
--- Tests/MojangAPITests/MojangAPITests.swift
+++ Tests/MojangAPITests/MojangAPITests.swift
@@ -4,27 +4,22 @@ import MojangAPI
 @Test
 func fetchManifest() async throws {
     let json = try await Mojang.fetchManifest()
-    try #require(json.latest.release.isEmpty == false)
-    try #require(json.versions.isEmpty == false)
-    try #require(json.versions.first?.url.isEmpty == false)
-}
-
-@Test(arguments: [
-    ("1.21.4", "a3bcba436caa849622fd7e1e5b89489ed6c9ac63"), // fetch game version package info
-    ("19", "87954e1d978096bd2cb1cd5a507a55fe25690a51"),     // fetch game version package assetIndex info
-])
-func fetchPackageInfo(id: String, sha1: String) async throws {
-    let response = try await Mojang.fetchPackage(id: id, sha1: sha1)
-    switch response {
-    case .GameVersion(let gameVersion):
-        try #require(gameVersion.arguments.game.isEmpty == false)
-        try #require(gameVersion.arguments.jvm.isEmpty == false)
-        try #require(gameVersion.assetIndex.url.isEmpty == false)
-        try #require(gameVersion.downloads.client.url.isEmpty == false)
-        try #require(gameVersion.downloads.server.url.isEmpty == false)
-        try #require(gameVersion.libraries.isEmpty == false)
-        try #require(gameVersion.mainClass.isEmpty == false)
-    case .GameVersionAsset(let asset):
-        try #require(asset.objects.value.isEmpty == false)
-    }
+    #expect(json.latest.release.isEmpty == false)
+    #expect(json.versions.isEmpty == false)
+    
+    let latestVersion = try #require(json.versions.first)
+    #expect(latestVersion.url.isEmpty == false)
+    #expect(latestVersion.sha1.isEmpty == false)
+    
+    let gameVersion = try #require(try await Mojang.gameVersion(of: latestVersion))
+    #expect(gameVersion.arguments.game.isEmpty == false)
+    #expect(gameVersion.arguments.jvm.isEmpty == false)
+    #expect(gameVersion.assetIndex.url.isEmpty == false)
+    #expect(gameVersion.downloads.client.url.isEmpty == false)
+    #expect(gameVersion.downloads.server.url.isEmpty == false)
+    #expect(gameVersion.libraries.isEmpty == false)
+    #expect(gameVersion.mainClass.isEmpty == false)
+    
+    let assetIndexObject = try #require(try await gameVersion.assetIndexObject)
+    #expect(assetIndexObject.objects.value.isEmpty == false)
 }
