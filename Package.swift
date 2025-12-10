3d7d814ef69fce81165f6680cfd878317b6ab294
wangzhizhou <wangzhizhou@bytedance.com>
2025-12-10 21:12:14 +0800

    update deps

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -10,30 +9,46 @@ let package = Package(
         .iOS(.v13),
         .tvOS(.v13),
         .watchOS(.v6),
-        .visionOS(.v1)
+        .visionOS(.v1),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MojangAPI",
-            targets: ["MojangAPI"]),
+            targets: ["MojangAPI"]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-openapi-generator.git", from: "1.9.0"),
-        .package(url: "https://github.com/apple/swift-openapi-runtime.git", from: "1.8.2"),
-        .package(url: "https://github.com/apple/swift-openapi-urlsession.git", from: "1.1.0")
+        .package(
+            url: "https://github.com/apple/swift-openapi-generator.git",
+            from: "1.10.3"
+        ),
+        .package(
+            url: "https://github.com/apple/swift-openapi-runtime.git",
+            from: "1.9.0"
+        ),
+        .package(
+            url: "https://github.com/apple/swift-openapi-urlsession.git",
+            from: "1.2.0"
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "MojangAPI",
             dependencies: [
-                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
-                .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
+                .product(
+                    name: "OpenAPIRuntime",
+                    package: "swift-openapi-runtime"
+                ),
+                .product(
+                    name: "OpenAPIURLSession",
+                    package: "swift-openapi-urlsession"
+                ),
             ],
             plugins: [
-                .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator"),
+                .plugin(
+                    name: "OpenAPIGenerator",
+                    package: "swift-openapi-generator"
+                )
             ]
         ),
         .testTarget(
